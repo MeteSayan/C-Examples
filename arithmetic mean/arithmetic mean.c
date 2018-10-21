b5320acce6b22e4cfdd34cded9e3a33f6cc1530d
Mete Sayan <metesayan99@gmail.com>
2018-10-21 13:16:02 +0300

    Test

---
+++ arithmetic mean/arithmetic mean.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+	
+	int a,b,c,d,e;
+	
+	float arith;
+	
+	printf("Enter 5 numbers:");
+	scanf("%d %d %d %d %d",&a,&b,&c,&d,&e);
+	arith = (a+b+c+d+e)/5.0;
+	printf("The arithmetic mean of the numbers you entered: %.2f",aritmetik);	
+
+	
+}
